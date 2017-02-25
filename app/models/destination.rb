8470e1a913f5d4cdf0b526729f767c1b1da8c461
Juan David Vargas <juandavidvlame12@hotmail.com>
2017-02-25 15:19:17 -0500

    funcionando subir imagenes, muchos a muchos y autenticacion

---
+++ app/models/destination.rb
@@ -0,0 +1,24 @@
+class Destination < ApplicationRecord
+	belongs_to :user
+	has_many :has_categories
+	has_many :categories, through: :has_categories
+
+	validates :place, presence: true, uniqueness: true
+	validates :why_go, presence: true
+
+	after_create :save_categories
+	has_attached_file :cover, styles: { medium: "1280x720", thumb: "800x600"}
+	validates_attachment_content_type :cover, content_type: /\Aimage\/.*\Z/
+
+	def categories=(value)
+		@categories = value
+	end
+
+	private
+
+	def save_categories
+		@categories.each do |category_id|
+			HasCategory.create(category_id: category_id,destination_id: self.id)
+		end
+	end
+end
---
+++ db/schema.rb
@@ -0,0 +1,70 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20170225190406) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "destinations", force: :cascade do |t|
+    t.string   "place"
+    t.text     "why_go"
+    t.text     "to_do"
+    t.text     "sights"
+    t.text     "festivals"
+    t.text     "sleep"
+    t.text     "eat"
+    t.text     "drink"
+    t.text     "shop"
+    t.text     "around"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.integer  "user_id"
+    t.string   "cover_file_name"
+    t.string   "cover_content_type"
+    t.integer  "cover_file_size"
+    t.datetime "cover_updated_at"
+    t.index ["user_id"], name: "index_destinations_on_user_id"
+  end
+
+  create_table "has_categories", force: :cascade do |t|
+    t.integer  "destination_id"
+    t.integer  "category_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.index ["category_id"], name: "index_has_categories_on_category_id"
+    t.index ["destination_id"], name: "index_has_categories_on_destination_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "name"
+    t.string   "permission_level"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+end
